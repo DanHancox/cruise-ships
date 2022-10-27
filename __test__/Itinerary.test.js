8c5d605f9075d6b7262c5c578dc929c36d7d7cbe
Dan Hancox <dthancox@gmail.com>
2022-10-27 20:39:27 +0100

    Itinerary and test added and passed with Ports

--- __test__/Itinerary.test.js
+++ __test__/Itinerary.test.js
@@ -5,4 +5,12 @@ describe('Itinerary', () => {
     it('can be instantiated', () => {
         expect(new Itinerary()).toBeInstanceOf(Object);
     })
+    it('can have Ports', () => {
+        const bristol = new Port('Bristol')
+        const estonia = new Port('Estonia')
+
+        const itinerary = new Itinerary([bristol, estonia]);
+        expect(itinerary.ports).toEqual([bristol, estonia]);
+
+    })
 })
