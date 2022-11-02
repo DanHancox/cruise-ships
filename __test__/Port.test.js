9077520e1a37b50c8091467536b076fb7c761e59
Dan Hancox <dthancox@gmail.com>
2022-11-02 15:54:20 +0000

    jest.fn working through

--- __test__/Port.test.js
+++ __test__/Port.test.js
@@ -1,16 +1,17 @@
+const Itinerary = require('../src/Itinerary');
 const Port = require('../src/Port');
+const Ship = require('../src/Ship');
 
 describe('Port', () => {
     describe('before', () => {
         let dover;
         let ship;
+        let itinerary;
         beforeEach(() => {
             dover = new Port("Dover");
-            ship = {};
-
-
-
-        })
+            itinerary = new Itinerary([dover])
+            ship = jest.fn;
+    })
     it('can be instantiated', () => {
         expect(new Port()).toBeInstanceOf(Object);
     })
@@ -18,15 +19,15 @@ describe('Port', () => {
         expect(dover.name.toString()).toBe("Dover");
     })
     it('Can add a ship', () => {
-
+        expect(dover.ships).toEqual([])
         dover.addShip(ship);
 
-        expect(dover.ships).toContain(ship);
+        expect(dover.ships.length).toEqual(1);
 
     })
     it('Can remove a ship', () => {
         
-        const titanic = {};
+        const titanic = jest.fn;
 
         dover.addShip(ship)
         dover.addShip(titanic)
